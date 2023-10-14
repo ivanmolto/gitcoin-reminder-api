56bd66738207bbc5a7c1bed65dc78412b55c402e
Ivan Molto <ivanmolto@gmail.com>
2023-10-14 19:01:02 +0200

    chore: Add Event model

--- app/events/models.py
+++ app/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models # noqa
 
-# Create your models here.
+
+class Event(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=False)
+    blurb = models.CharField(max_length=200, blank=False)
+    url = models.URLField(max_length=80, blank=True)
+    start_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['start_date']
