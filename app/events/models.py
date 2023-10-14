692e330b55ae94a57331b7dfaa62f659c0055b01
Ivan Molto <ivanmolto@gmail.com>
2023-10-15 00:01:40 +0200

    chore: Update Event model

--- app/events/models.py
+++ app/events/models.py
@@ -4,7 +4,7 @@ from django.db import models # noqa
 class Event(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     event_name = models.CharField(max_length=100, blank=False)
-    cta_text = models.CharField(max_length=280, blank=False)
+    cta_text = models.TextField(max_length=280, blank=False)
     bot_name = models.CharField(max_length=15, blank=False)
     event_date = models.DateTimeField()
 
