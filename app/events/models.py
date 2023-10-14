3872d612fbd6de89f0acb97ee570ac32bd0bdb6c
Ivan Molto <ivanmolto@gmail.com>
2023-10-14 23:35:14 +0200

    chore: Update Event model

--- app/events/models.py
+++ app/events/models.py
@@ -2,14 +2,14 @@ from django.db import models # noqa
 
 
 class Event(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=False)
-    blurb = models.CharField(max_length=200, blank=False)
-    url = models.URLField(max_length=80, blank=True)
-    start_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    event_name = models.CharField(max_length=100, blank=False)
+    cta_text = models.CharField(max_length=280, blank=False)
+    bot_name = models.CharField(max_length=15, blank=False)
+    event_date = models.DateTimeField()
 
     def __str__(self):
-        return self.title
+        return self.event_name
 
     class Meta:
-        ordering = ['start_date']
+        ordering = ['event_date']
