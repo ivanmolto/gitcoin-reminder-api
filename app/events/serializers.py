be82c9c6cc34210d4912fe4fa646704d597f6cb7
Ivan Molto <ivanmolto@gmail.com>
2023-10-16 17:42:01 +0200

    chore: Update Event serializer

--- app/events/serializers.py
+++ app/events/serializers.py
@@ -1,8 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from events.models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Event
-        fields = ['id', 'event_name', 'cta_text', 'bot_name', 'event_date']
+        fields = ['id', 'owner', 'event_name', 'cta_text', 
+                  'bot_name',
+                  'event_date']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    events = serializers.PrimaryKeyRelatedField(many=True, 
+                                                queryset=Event.objects.all()
+                                                )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'events']
+
