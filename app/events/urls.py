b74e9aa5df1e15fefa3224474be008215f68f81d
Ivan Molto <ivanmolto@gmail.com>
2023-10-16 17:44:14 +0200

    chore: Update views and urls

--- app/events/urls.py
+++ app/events/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from events import views
 
+
 urlpatterns = [
-  path('events/', views.event_list),
-  path('events/<int:pk>/', views.event_detail),
+    path('events/', views.EventList.as_view()),
+    path('events/<int:pk>/', views.EventDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
+
+urlpatterns += [
+  path('api-auth/', include('rest_framework.urls'))
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- app/events/views.py
+++ app/events/views.py
@@ -1,51 +1,31 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.contrib.auth.models import User
 from events.models import Event
-from events.serializers import EventSerializer
+from events.serializers import EventSerializer, UserSerializer
+from rest_framework import generics, permissions
+from events.permissions import IsOwnerOrReadOnly
 
 
-@csrf_exempt
-def event_list(request):
-    """
-    List all events, or create a new event.
-    """
-    if request.method == 'GET':
-        events = Event.objects.all()
-        serializer = EventSerializer(events, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class EventList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = EventSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-@csrf_exempt
-def event_detail(request, pk):
-    """
-    Retrieve, update or delete an event.
-    """
-    try:
-        event = Event.objects.get(pk=pk)
-    except Event.DoesNotExist:
-        return HttpResponse(status=404)
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
-    if request.method == 'GET':
-        serializer = EventSerializer(event)
-        return JsonResponse(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = EventSerializer(event, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    elif request.method == 'DELETE':
-        event.delete()
-        return HttpResponse(status=204)
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
